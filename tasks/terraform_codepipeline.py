36f59c32b63bcace79ac3a8da705159650094e61
James Fogarty <jayfo@users.noreply.github.com>
2024-03-20 07:43:31 -0700

    Apply Variables in CodeBuild

--- tasks/terraform_codepipeline.py
+++ tasks/terraform_codepipeline.py
@@ -1,12 +1,15 @@
-from invoke import Collection, task
+from invoke import Collection, Context, task
 import os
 import os.path
 from pathlib import Path
+import ruamel.yaml
 import shutil
+from typing import Dict
 
 from tasks.constants import (
     CODEBUILD_NAME,
     CODEPIPELINE_NAME,
+    ECR_REPOSITORY_NAME,
     GIT_REPOSITORY_ID,
     GIT_REPOSITORY_BRANCH,
     PATH_CODEBUILD_SOURCE,
@@ -16,11 +19,21 @@ from tasks.constants import (
     PATH_STAGING_CODEBUILD_SOURCE,
     PATH_TERRAFORM_BIN,
 )
+from tasks.terraform_ecr import TerraformOutputEcr
 from tasks.terraform import write_terraform_variables
 
 
 PATH_TERRAFORM_DIR = Path("./terraform/codepipeline")
 PATH_STAGING_TERRAFORM_VARIABLES = Path(PATH_STAGING, "terraform/codepipeline.tfvars")
+
+
+def codebuild_environment_variables(*, context: Context) -> Dict[str, str]:
+    with TerraformOutputEcr(context) as ecr:
+        return {
+            "ECR_REGISTRY_URL": ecr.output.registry_url,
+            "ECR_REPOSITORY": ECR_REPOSITORY_NAME,
+            "ECR_REPOSITORY_URL": ecr.output.repository_urls[ECR_REPOSITORY_NAME],
+        }
 
 
 @task
@@ -34,6 +47,35 @@ def task_create_codebuild_archive(context):
 
     # Copy archive source into a staging directory
     shutil.copytree(src=PATH_CODEBUILD_SOURCE, dst=PATH_STAGING_CODEBUILD_SOURCE)
+
+    # Obtain the variables we need to update in the buildspec.yaml
+    dynamic_variables = codebuild_environment_variables(context=context)
+
+    # Obtain the path to the buildspec.yaml file we need to enhance
+    buildspec_path = Path(PATH_STAGING_CODEBUILD_SOURCE, "buildspec.yaml")
+
+    # Use a parsing object for roundtrip
+    # Invoking a parse without keeping the object will not maintain state for round trip
+    yaml_parser = ruamel.yaml.YAML()
+
+    # Update the buildspec to add provided environment variables
+    with open(buildspec_path) as file_buildspec:
+        yaml_buildspec = yaml_parser.load(file_buildspec)
+
+    # Ensure the buildspec provides for environment variables
+    if "env" not in yaml_buildspec:
+        yaml_buildspec["env"] = {}
+    if "variables" not in yaml_buildspec["env"]:
+        yaml_buildspec["env"]["variables"] = {}
+
+    # Add the variables
+    for key_current, value_current in dynamic_variables.items():
+        yaml_buildspec["env"]["variables"][key_current] = value_current
+
+    # Replace the buildspec
+    os.remove(buildspec_path)
+    with open(buildspec_path, mode="w") as file_buildspec:
+        yaml_parser.dump(yaml_buildspec, file_buildspec)
 
     # Make the archive
     shutil.make_archive(
