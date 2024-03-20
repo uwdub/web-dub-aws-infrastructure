5b137f81b156fb467386d859d29ecaee46f23960
James Fogarty <jayfo@users.noreply.github.com>
2024-03-20 07:38:37 -0700

    Additional Network and ECS Constants

--- tasks/constants.py
+++ tasks/constants.py
@@ -5,6 +5,19 @@ CODEBUILD_NAME = "web-dub-codebuild"
 GIT_REPOSITORY_ID = "uwdub/web-dub"
 GIT_REPOSITORY_BRANCH = "master"
 
+ECR_REPOSITORY_NAME = "uwdub/web-dub"
+
+ECS_TASK_NAME = "web-dub"
+
+NETWORK_AVAILABILITY_ZONES = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+    "us-east-1d",
+    "us-east-1e",
+    "us-east-1f",
+]
+
 PATH_CODEBUILD_SOURCE = Path("./codebuild")
 
 PATH_STAGING = Path("./.staging")
