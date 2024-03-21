23bd573af18895efd6046cdfec8d7751d5877825
James Fogarty <jayfo@users.noreply.github.com>
2024-03-21 05:18:42 -0700

    Fix Destroy All Task

--- tasks/terraform_all.py
+++ tasks/terraform_all.py
@@ -7,7 +7,7 @@ import tasks.terraform_ecr
 import tasks.terraform_ecs
 import tasks.terraform_network
 
-TASK_ORDER = [
+TASK_ORDER_APPLY = [
     tasks.terraform_backend.task_terraform_apply,
     tasks.terraform_ecr.task_terraform_apply,
     tasks.terraform_network.task_terraform_apply,
@@ -16,8 +16,17 @@ TASK_ORDER = [
     tasks.terraform_ecs.task_terraform_apply,
 ]
 
+TASK_ORDER_DESTROY = [
+    tasks.terraform_ecs.task_terraform_destroy,
+    tasks.terraform_codepipeline.task_terraform_destroy,
+    tasks.terraform_alb.task_terraform_destroy,
+    tasks.terraform_network.task_terraform_destroy,
+    tasks.terraform_ecr.task_terraform_destroy,
+    tasks.terraform_backend.task_terraform_destroy,
+]
 
-@task(pre=TASK_ORDER)
+
+@task(pre=TASK_ORDER_APPLY)
 def task_terraform_apply(context):
     """
     Issue all Terraform apply.
@@ -25,7 +34,7 @@ def task_terraform_apply(context):
     pass
 
 
-@task(pre=reversed(TASK_ORDER))
+@task(pre=TASK_ORDER_DESTROY)
 def task_terraform_destroy(context):
     """
     Issue all Terraform destroy.
