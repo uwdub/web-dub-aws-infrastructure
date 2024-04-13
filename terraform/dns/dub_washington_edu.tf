8e990a989e1092ac31c25465e64e5f736bb4e29d
James Fogarty <jayfo@users.noreply.github.com>
2024-04-13 11:06:35 -0700

    Add SPF and DKIM Records

--- terraform/dns/dub_washington_edu.tf
+++ terraform/dns/dub_washington_edu.tf
@@ -8,7 +8,7 @@ resource "aws_route53_zone" "dub_washington_edu" {
 
 resource "aws_route53_record" "dub_washington_edu_alb_alias" {
   zone_id = aws_route53_zone.dub_washington_edu.zone_id
-  name    = "dub.washington.edu"
+  name    = "dub.washington.edu."
   type    = "A"
 
   alias {
@@ -19,7 +19,7 @@ resource "aws_route53_record" "dub_washington_edu_alb_alias" {
   }
 }
 
-resource "aws_route53_record" "dub_washington_edu_validation" {
+resource "aws_route53_record" "dub_washington_edu_ssl_validation" {
   zone_id = aws_route53_zone.dub_washington_edu.zone_id
   name    = "_a210a9a20344038976007c7f51423d10.dub.washington.edu."
   type    = "CNAME"
@@ -33,7 +33,7 @@ resource "aws_route53_record" "dub_washington_edu_validation" {
 
 resource "aws_route53_record" "dub_washington_edu_mx_dubber" {
   zone_id = aws_route53_zone.dub_washington_edu.zone_id
-  name    = "dub.washington.edu"
+  name    = "dub.washington.edu."
   type    = "MX"
 
   records = [
@@ -41,4 +41,30 @@ resource "aws_route53_record" "dub_washington_edu_mx_dubber" {
   ]
 
   ttl = "86400"
+}
+
+resource "aws_route53_record" "dub_washington_edu_spf_dubber" {
+  zone_id = aws_route53_zone.dub_washington_edu.zone_id
+  name    = "dub.washington.edu."
+  type    = "TXT"
+
+  records = [
+    "v=spf1 include:spf.cs.uw.edu ~all"
+  ]
+
+  ttl = "600"
+}
+
+resource "aws_route53_record" "dub_washington_edu_dkim_dubber" {
+  zone_id = aws_route53_zone.dub_washington_edu.zone_id
+  name    = "20240410._domainkey.dub.washington.edu."
+  type    = "TXT"
+
+  records = [
+    "v=DKIM1; h=rsa-sha256; k=rsa;",
+    "p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4Upk03L89MiLDZSLwmjIMRvVbPpmjlB7R0PdRgHMLpYNIqHGCY5oayM5mYKHB3Gz9VNWVBfuQtTKe4Hd+ITzMfB/QLF8Cs32ja+urrORw2dpwAznhDG70hGIQ8m606QULLk1OcZhHsMqe5xcBCEI1E19EzP6GhOqaVHzJUClRxrsWXKJ1FKzFzjBvKN9k9grh57Hj373/RxnIs",
+    "/qLlnm64EoDZJt0NkKX/1IRunQZl0BK3HrrZq3LmGzWVRT4GlZIJJ35uUjXBoAZfEUzAgLHu3n3BFpxHA066iRWQDQMeIASp934z6JJohK0D9j19CqEchWCj3/IhZ+MUcBG4Ej7QIDAQAB"
+  ]
+
+  ttl = "600"
 }
--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -1,7 +1,13 @@
+/*
+ * Used for manually providing UW with name servers for NS records.
+ */
 output "dub_uw_edu" {
   value = aws_route53_zone.dub_uw_edu
 }
 
+/*
+ * Used for manually providing UW with name servers for NS records.
+ */
 output "dub_washington_edu" {
   value = aws_route53_zone.dub_washington_edu
 }
