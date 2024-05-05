93d65f935f4bb0b1506d1fc31b282b13039cfadd
James Fogarty <jayfo@users.noreply.github.com>
2024-05-05 07:06:35 -0700

    Adjust Sizing of Availability Zones

--- tasks/constants.py
+++ tasks/constants.py
@@ -10,13 +10,18 @@ ECR_REPOSITORY_NAME = "uwdub/web-dub"
 
 ECS_TASK_NAME = "web-dub"
 
+# Each availability zone results in a public IP,
+# associated with the application load balancer.
+# Billed at $.005 per hour, each public IP is ~$3 per month.
+# DUB website does not need that over-redundancy.
+# ALB requires a minimum of two availability zones.
 NETWORK_AVAILABILITY_ZONES = [
     "us-east-1a",
-    "us-east-1b",
-    "us-east-1c",
+    # "us-east-1b",
+    # "us-east-1c",
     "us-east-1d",
-    "us-east-1e",
-    "us-east-1f",
+    # "us-east-1e",
+    # "us-east-1f",
 ]
 
 PATH_CODEBUILD_SOURCE = Path("./codebuild")
